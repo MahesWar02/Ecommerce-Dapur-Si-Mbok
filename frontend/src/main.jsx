a9fe5f459e7f25b76b6245de0e8dd7948f0d728a
MahesWar02 <staabhi71@gmail.com>
2026-06-08 11:31:28 +0700

    fix: persist auth state on refresh

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,18 +3,21 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { PersistGate } from "redux-persist/integration/react";
 import "react-toastify/dist/ReactToastify.css";
-import { store } from "./store/index";
+import { store, persistor } from "./store/index";
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-        <ToastContainer position="top-right" autoClose={3000} />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+          <ToastContainer position="top-right" autoClose={3000} />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 );
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import authReducer from "./slices/authSlice";
 import productReducer from "./slices/productSlice";
 import cartReducer from "./slices/cartSlice";
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    product: productReducer,
-    cart: cartReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"],
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  product: productReducer,
+  cart: cartReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);
